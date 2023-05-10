3045b52232f37d62ec2edce10625a4055457e132
GabrielPaez <darkdelll@gmail.com>
2023-05-10 18:26:25 -0400

    cambios en el header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div class="header">
+        <section class="header_logo">
+            <article>Sofia soza</article>
+            <article>Marketing Media Solutions</article>
+        </section>
+        <section class="header_nav">
+            <ul>
+                <li>¿QUIEN SOY?</li>
+                <li>SERVICIOS</li>
+                <li>PROYECTOS</li>
+                <li>CONTACTO</li>
+            </ul>
+        </section>
+    </div>
   )
 }
 
