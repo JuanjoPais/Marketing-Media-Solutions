b8bcce5395edfb8f17a296802a2663682870572e
Juan Pais <jjosepais@gmail.com>
2023-05-12 00:05:08 -0300

    Empiezo con servicios, falta terminar los recuadros

--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -1,20 +1,40 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import {useNavigate} from "react-router-dom";
 
 const Servicios = () => {
-    const navigate = useNavigate()
-    const onMouseWheel = (e)=>{
-    if(e.deltaY>0){
-        navigate("/proyectos")
-    }else{
-        navigate("/main")
-    }
-}
-  return (
-    <div class="servicios" onWheel={onMouseWheel}>
-        Servicios
-    </div>
-  )
-}
+	const navigate = useNavigate();
+	const onMouseWheel = (e) => {
+		if (e.deltaY > 0) {
+			navigate("/proyectos");
+		} else {
+			navigate("/main");
+		}
+	};
+	return (
+		<div class="servicios" onWheel={onMouseWheel}>
+			<h2 className="servicios_titulo">Servicios</h2>
+			<p className="servicios_texto">
+				En <span className="servicios_marca">Marketing Media Solutions</span>,
+				trabajamos en estrecha colaboración de nuestros clientes para entender
+				sus necesidades, desarrollar estrategias personalizadas y reflejar su
+				esencia única.
+			</p>
+			<section className="servicios_contenedorRecuadros">
+				<div className="servicios_recuadro">
+					Comunnity
+					<br />
+					Manager
+				</div>
+				<div className="servicios_recuadro">Asesorías</div>
+				<div className="servicios_recuadro">
+					Manual de
+					<br />
+					marca
+				</div>
+				<div className="servicios_recuadro">Anuncios</div>
+			</section>
+		</div>
+	);
+};
 
-export default Servicios
+export default Servicios;
