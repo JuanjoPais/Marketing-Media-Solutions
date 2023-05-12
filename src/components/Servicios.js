d9d3a9af212d69b4cee56dc78cef6c91ee67bb11
GabrielPaez <darkdelll@gmail.com>
2023-05-12 01:06:58 -0400

    nuevas animaciones, box shadow y letter spacing

--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -21,18 +21,18 @@ const Servicios = () => {
 				esencia única.
 			</p>
 			<section className="servicios_contenedorRecuadros">
-				<div className="servicios_recuadro">
+				<div className="servicios_recuadro" id="frame1">
 					Comunnity
 					<br />
 					Manager
 				</div>
-				<div className="servicios_recuadro">Asesorías</div>
-				<div className="servicios_recuadro">
+				<div className="servicios_recuadro" id="frame2">Asesorías</div>
+				<div className="servicios_recuadro" id="frame3">
 					Manual de
 					<br />
 					marca
 				</div>
-				<div className="servicios_recuadro">Anuncios</div>
+				<div className="servicios_recuadro" id="frame4">Anuncios</div>
 			</section>
 			<Contacto />
 		</div>
