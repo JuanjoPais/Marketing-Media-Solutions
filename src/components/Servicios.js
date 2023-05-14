d61637c852b932979c393e1b0e231ec1a337f3b3
Juan Pais <jjosepais@gmail.com>
2023-05-14 00:19:15 -0300

    termino textos del evento hover en Servicios. Falta acomodar el texto y el efecto de expansión

--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -4,7 +4,10 @@ import Contacto from "./Contacto";
 import {useState} from "react";
 
 const Servicios = () => {
-	const [texto, setTexto] = useState("Community Manager");
+	const [texto1, setTexto1] = useState("Community Manager");
+	const [texto2, setTexto2] = useState("Asesorias");
+	const [texto3, setTexto3] = useState("Manual de marca");
+	const [texto4, setTexto4] = useState("Anuncios");
 	const navigate = useNavigate();
 	const onMouseWheel = (e) => {
 		if (e.deltaY > 0) {
@@ -13,8 +16,10 @@ const Servicios = () => {
 			navigate("/main");
 		}
 	};
-	const handleMouseOver = () => {
-		setTexto(
+
+	//todo esto de abajo debería ir en otro componente creo. Queda muy largo. También estoy repitiendo mucho código.
+	const handleMouseOverFrame1 = () => {
+		return setTexto1(
 			<ul className="servicios_textoHover">
 				<li className="servicios_itemsHover">
 					Calendarios semanales y mensuales
@@ -28,9 +33,51 @@ const Servicios = () => {
 		);
 	};
 
-	const handleMouseLeave = () => {
-		setTexto("Community Manager");
+	const handleMouseLeaveFrame1 = () => {
+		setTexto1("Community Manager");
 	};
+
+	const handleMouseOverFrame2 = () => {
+		return setTexto2(
+			<ul className="servicios_textoHover">
+				<li className="servicios_itemsHover">
+					Estrategias efectivas de redes sociales
+				</li>
+				<li className="servicios_itemsHover">Aumentar su presencia en línea</li>
+				<li className="servicios_itemsHover">
+					Mejorar su interacción con los clientes
+				</li>
+				<li className="servicios_itemsHover">Aumentar su alcance</li>
+			</ul>
+		);
+	};
+	const handleMouseLeaveFrame2 = () => {
+		setTexto2("Asesorias");
+	};
+
+	const handleMouseOverFrame3 = () => {
+		return setTexto3(
+			<p className="servicios_textoHover">
+				Tipografía, colores que nos definen, logos y sus usos adecuados.
+			</p>
+		);
+	};
+	const handleMouseLeaveFrame3 = () => {
+		setTexto3("Manual de marca");
+	};
+	const handleMouseOverFrame4 = () => {
+		return setTexto4(
+			<p className="servicios_textoHover">
+				Promover la marca y alcanzar los objetivos de marketing a través de
+				estrategias publicitarias en redes sociales.
+			</p>
+		);
+	};
+	const handleMouseLeaveFrame4 = () => {
+		setTexto4("Manual de marca");
+	};
+
+	//Hasta acá, todo lo previo mandarlo a otro componente.
 
 	return (
 		<div class="servicios" onWheel={onMouseWheel}>
@@ -43,23 +90,36 @@ const Servicios = () => {
 			</p>
 			<section className="servicios_contenedorRecuadros">
 				<div
-					onMouseOver={handleMouseOver}
-					onMouseLeave={handleMouseLeave}
+					onMouseOver={handleMouseOverFrame1}
+					onMouseLeave={handleMouseLeaveFrame1}
 					className="servicios_recuadro"
 					id="frame1"
 				>
-					{texto}
+					{texto1}
 				</div>
-				<div className="servicios_recuadro" id="frame2">
-					Asesorías
+				<div
+					onMouseOver={handleMouseOverFrame2}
+					onMouseLeave={handleMouseLeaveFrame2}
+					className="servicios_recuadro"
+					id="frame2"
+				>
+					{texto2}
 				</div>
-				<div className="servicios_recuadro" id="frame3">
-					Manual de
-					<br />
-					marca
+				<div
+					onMouseOver={handleMouseOverFrame3}
+					onMouseLeave={handleMouseLeaveFrame3}
+					className="servicios_recuadro"
+					id="frame3"
+				>
+					{texto3}
 				</div>
-				<div className="servicios_recuadro" id="frame4">
-					Anuncios
+				<div
+					onMouseOver={handleMouseOverFrame4}
+					onMouseLeave={handleMouseLeaveFrame4}
+					className="servicios_recuadro"
+					id="frame4"
+				>
+					{texto4}
 				</div>
 			</section>
 			<Contacto />
