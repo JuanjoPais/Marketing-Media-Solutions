404622c9520e5631f5f3d1cc760a7caaef71fad5
Juan Pais <jjosepais@gmail.com>
2023-05-17 23:21:40 -0300

    edito btn wasap

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import Whatsapplogo from './WhatsApp_icon.png'
+import React from "react";
+import Vector from "./Vector.png";
 
 const Contacto = () => {
-  return (
-    <div class="Contacto">
-        <img src={Whatsapplogo} alt="logo de WhatsApp"></img>
-        <p>Hola ¿Consultas?</p>
-    </div>
-  )
-}
+	return (
+		<div class="Contacto">
+			<img src={Vector} alt="logo de WhatsApp"></img>
+			<p>Hola! ¿Consultas?</p>
+		</div>
+	);
+};
 
-export default Contacto
+export default Contacto;
